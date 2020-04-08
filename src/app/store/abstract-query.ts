161cb41083e0abfdf2c6ce12db92a045e5568028
jack <jack@qec.com>
2020-04-08 18:18:00 -0500

    add abstract query

---
+++ src/app/store/abstract-query.ts
@@ -0,0 +1,16 @@
+import {Observable} from 'rxjs';
+import {distinctUntilChanged, map} from 'rxjs/operators';
+import {Store} from './abstract-store';
+
+export abstract class Query<S> {
+
+    protected constructor(public store: Store<S>) {
+    }
+
+    select<T>(selector: (state: S) => T, comparator?: (a: T, b: T) => boolean): Observable<T> {
+        return this.store.$state.pipe(
+            map(selector),
+            distinctUntilChanged(comparator)
+        );
+    }
+}
